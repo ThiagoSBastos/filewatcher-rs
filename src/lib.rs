d748a1b2e1eb2beeca011051cedc7aeb6c531b4d
tbastos <thiagosbastos@live.com>
2023-11-04 21:15:28 +0100

    refactor: improve path handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-use std::fmt;
 use std::fs;
 use std::collections::HashMap;
+use std::io;
 use std::path::PathBuf;
 
 #[derive(Debug)]
 pub struct FileWatcher {
-    dirname: String,
+    dirname: PathBuf,
     filemap: HashMap<String, FileEvent>,
 }
 
@@ -16,39 +16,49 @@ pub enum FileEvent {
     Deleted,
 }
 
-impl fmt::Display for FileEvent {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            FileEvent::Created => write!(f, "FileEvent::Created"),
-            FileEvent::Updated => write!(f, "FileEvent::Updated"),
-            FileEvent::Deleted => write!(f, "FileEvent::Deleted"),
-        }
-    }
-}
-
 impl FileWatcher {
-    pub fn new (pathname: String) -> std::io::Result<Self> {  
+    fn get_files (pathname: &PathBuf) -> io::Result<Vec<PathBuf>>{
+        let mut filenames = vec![];
+        let entries = fs::read_dir(&pathname)?;
+        for entry in entries {
+            let entry = entry?;
+            let meta = entry.metadata()?;
+            if meta.is_dir() {
+                let mut subdir = Self::get_files(&entry.path())?;
+                filenames.append(&mut subdir);
+            }
 
-        let _ = fs::metadata(&pathname)?;
+            if meta.is_file() {
+                filenames.push(entry.path());
+            }
+        }
+
+
+        return Ok(filenames);
+    }
+
+    // Registers all files of a directory as Created for the FileWatcher
+    pub fn new (pathname: PathBuf) -> io::Result<Self> {
+        let filenames = Self::get_files(&pathname)?;
 
         let mut fmap = HashMap::new();
-        let filenames: Vec<PathBuf>= fs::read_dir(&pathname)?
-            .into_iter()
-            .map(|r| r.unwrap().path())
-            .filter(|r| r.is_file())
-            .collect();
-
         for el in filenames {
-            fmap.insert(el.into_os_string().into_string().unwrap(), FileEvent::Created);       
+            fmap.insert(el.into_os_string().into_string().unwrap(), FileEvent::Created);
         }
 
         Ok(FileWatcher{dirname: pathname, filemap: fmap})
     }
 
-    pub fn watch(&self, _action: impl FnOnce(FileEvent)) -> std::io::Result<()>{
+    pub fn watch(&self, _action: impl FnOnce(FileEvent)) -> io::Result<()>{
 
-        println!("Action performed on {}", self.dirname);
-        for (key,value) in &self.filemap { println!("key: {}, value: {}", key, value); }
+        println!("Action performed on {}", self.dirname.as_path().display().to_string());
+        for (key,value) in &self.filemap { println!("key: {}, value: {:?}", key, value); }
+
+        // loop {
+        // make a state machine before any action has been done to check the current state of the
+        // file
+        // action();
+        // }
 
         Ok(())
     }
@@ -62,8 +72,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() -> std::io::Result<()> {
-        let pathname = String::from("/home/thiago/Documents/Code/file-watcher-rs/file-watcher/test_data");
+    fn it_works() -> io::Result<()> {
+        let pathname: PathBuf = PathBuf::from("test_data");
         FileWatcher::new(pathname)?.watch(|file_event| {
             match file_event {
                 FileEvent::Created => println!("Action for created"),
